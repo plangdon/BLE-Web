4b3e7de53401906a9b307b7c0c3f3f06b6407325
plangdon <plangdon317@gmail.com>
2017-03-08 13:10:40 -0500

    Create relayClick.js

---
+++ example/lendmeyourears/relayClick.js
@@ -0,0 +1,67 @@
+(function() {
+  'use strict';
+
+  class RelayClick {
+
+    /**
+     * customize your project here to reflect the uuid of your service and characteristics.
+     */
+    constructor() {
+        this.deviceName = 'katie';
+        this.serviceUUID = '917649a0-d98e-11e5-9eec-0002a5d5c51b';
+        this.characteristic1UUID = '917649a1-d98e-11e5-9eec-0002a5d5c51b';
+        this.device = null;
+        this.server = null;
+        // The cache allows us to hold on to characeristics for access in response to user commands 
+        this._characteristics = new Map();
+    }
+
+    connect(){
+        return navigator.bluetooth.requestDevice({
+         filters: [{
+          services:[this.serviceUUID]
+         }]
+        })
+        .then(device => {
+            this.device = device;
+            return device.gatt.connect();
+        })
+        .then(server => {
+            this.server = server;
+            return Promise.all([
+              server.getPrimaryService(this.serviceUUID)
+              .then(service=>{
+                return Promise.all([
+                  this._cacheCharacteristic(service, this.characteristic1UUID),
+                  // this._cacheCharacteristic(service, 'uuidCharacteristic2Here'),
+                ])
+              })
+            ]);
+        })
+    }
+
+  _cacheCharacteristic(service, characteristicUuid){
+    return service.getCharacteristic(characteristicUuid)
+    .then(characteristic => {
+      this._characteristics.set(characteristicUuid, characteristic);
+    });
+  }
+
+ _readCharacteristic(characteristicUuid) {
+   let characteristic = this._characteristics.get(characteristicUuid);
+   return characteristic.readValue()
+   .then(value => {
+     value - value.buffer ? value : new DataView(value);
+     return value;
+   });
+
+ }
+ _writeCharacteristic(characteristicUuid, value){
+   let characteristic = this._characteristics.get(characteristicUuid);
+   return characteristic.writeValue(value);
+ }
+}
+
+window.relayClick = new RelayClick();
+
+})();
